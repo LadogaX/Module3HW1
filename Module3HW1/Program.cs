0d90d74e94155cb503241f415fad9559aab588a6
LadogaX <ladogax@gmail.com>
2021-12-06 10:45:48 +0200

    First load

--- Module3HW1/Program.cs
+++ Module3HW1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using Module3HW1.Models;
+using Module3HW1.Services.Comparers;
+using Module3HW1.Helpers;
 
 namespace Module3HW1
 {
@@ -6,6 +10,30 @@ namespace Module3HW1
     {
         public static void Main(string[] args)
         {
+            var customList = new CustomList<int>();
+            var customList2 = new CustomList<Product>();
+
+            customList2.Add(new Product { Id = 1, Name = "Potato" });
+            customList2.Add(new Product { Id = 2, Name = "Tomato" });
+
+            customList2.AddRange(new Product[]
+            {
+                new Product { Id = 3, Name = "Carrot" },
+                new Product { Id = 4, Name = "Garlic" }
+            });
+
+            customList2.AddRange(customList2);
+            CustomListHelpers.Display(customList2, "Adding in collection");
+
+            customList2.Sort(new ProductComparer());
+            CustomListHelpers.Display(customList2, "Sorting:");
+
+            var result = customList2.RemoveAt(3);
+
+            result = customList2.Remove(delegate(Product product) { return product.Name.CompareTo("Tomato") == 0; });
+            customList2.Trim();
+
+            CustomListHelpers.Display(customList2, "State after remove:");
         }
     }
 }
